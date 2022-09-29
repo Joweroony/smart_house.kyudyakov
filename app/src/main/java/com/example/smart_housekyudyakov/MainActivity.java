4b80512876c06d38e58356806e217fe0f38f6958
Timur <timmy_1101@mail.ru>
2022-09-29 15:35:15 +0900

    pannels was hided

--- app/src/main/java/com/example/smart_housekyudyakov/MainActivity.java
+++ app/src/main/java/com/example/smart_housekyudyakov/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window W = getWindow();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //Скрываем нижнюю панель навигации
+        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //Появляется поверх игры и исчезает
         setContentView(R.layout.enter_your_house);
     }
 
@@ -18,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, new_resident.class);
         startActivity(intent);
     }
+
+
 
 }
--- app/src/main/java/com/example/smart_housekyudyakov/new_resident.java
+++ app/src/main/java/com/example/smart_housekyudyakov/new_resident.java
@@ -3,12 +3,18 @@ package com.example.smart_housekyudyakov;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class new_resident extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window W = getWindow();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //Скрываем нижнюю панель навигации
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //Появляется поверх игры и исчезает
+        setContentView(R.layout.enter_your_house);
         setContentView(R.layout.activity_new_resident);
     }
 }
