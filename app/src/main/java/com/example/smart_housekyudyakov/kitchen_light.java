40bc123c2aee3e33911174b1bb417f128eb7d855
timur <timmy_1101@mail.ru>
2022-10-31 13:54:03 +0900

    commit 14.10.22

---
+++ app/src/main/java/com/example/smart_housekyudyakov/kitchen_light.java
@@ -0,0 +1,14 @@
+package com.example.smart_housekyudyakov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kitchen_light extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen_light);
+    }
+}
